a6b4a756d4bfec9bdd8e332e379228c1c0096e07
lplummer <lplummer@restdevices>
2012-08-05 13:09:54 -0700

    test?

---
+++ website/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('website.views',
+    # Examples:
+    url(r'^$', 'index'),
+)
